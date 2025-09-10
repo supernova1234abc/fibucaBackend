d304a9428bde0ebed7bdba7207f34241c853aee1
unknown <yehoshaphatij@gmail.com>
2025-09-10 15:02:05 +0300

    add linux start venv and server cmd variable

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# activate the virtual environment
+source venv/bin/activate   
+echo "Virtual environment activated."
+# start the backend server
+echo "Starting the backend server..."
+npm run dev
